aebf005a9cd1fb03487ba71f731b203fa65d4ec4
rafiqulshopon <rafiqul.dev@gmail.com>
2023-10-13 23:43:34 +0600

    added new fields to signup

--- auth/auth.routes.js
+++ auth/auth.routes.js
@@ -1,28 +1,60 @@
 import express from 'express';
 import User from '../models/user.model.js';
-import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const {
+      email,
+      password,
+      name,
+      department,
+      studentId,
+      batch,
+      dateOfBirth,
+      phoneNumber,
+      address,
+    } = req.body;
 
     const existingUser = await User.findOne({ email });
-    if (existingUser)
+    if (existingUser) {
       return res.status(400).json({ message: 'User already exists.' });
+    }
 
-    const newUser = new User({ email, password });
+    const newUser = new User({
+      email,
+      password,
+      name,
+      department,
+      studentId,
+      batch,
+      dateOfBirth,
+      phoneNumber,
+      address,
+    });
     await newUser.save();
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: '1h',
-    });
+    const token = jwt.sign(
+      {
+        id: newUser._id,
+        name: newUser.name,
+        department: newUser.department,
+        batch: newUser.batch,
+        studentId: newUser.studentId,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '5h',
+      }
+    );
 
     res.status(201).json({ token });
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong.' });
+    res
+      .status(500)
+      .json({ message: 'Something went wrong.', error: error.message });
   }
 });
 
@@ -31,20 +63,34 @@ router.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
-    if (!existingUser)
+    if (!existingUser) {
       return res.status(404).json({ message: "User doesn't exist." });
+    }
 
     const isPasswordValid = await existingUser.isValidPassword(password);
-    if (!isPasswordValid)
+    if (!isPasswordValid) {
       return res.status(400).json({ message: 'Invalid password.' });
+    }
 
-    const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET, {
-      expiresIn: '1h',
-    });
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+        name: existingUser.name,
+        department: existingUser.department,
+        batch: existingUser.batch,
+        studentId: existingUser.studentId,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '5h',
+      }
+    );
 
     res.status(200).json({ token });
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong.' });
+    res
+      .status(500)
+      .json({ message: 'Something went wrong.', error: error.message });
   }
 });
 
