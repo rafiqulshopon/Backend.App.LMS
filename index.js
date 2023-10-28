b6f1cc94cc6a0ddd1563e61c10a883eba66eaa90
Rafiqul Shopon <rafiqul.dev@gmail.com>
2023-10-28 13:07:41 +0600

    convert books graphql apis to rest apis

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import typeDefs from './schemas/index.schema.js';
-import resolvers from './resolvers/index.resolver.js';
 import authRoutes from './auth/auth.routes.js';
+import { addBook, editBook, getBooks } from './controllers/book.controller.js';
 
 dotenv.config();
 
@@ -21,58 +17,43 @@ const SECRET_KEY = process.env.JWT_SECRET;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Middleware to set context
+app.use((req, res, next) => {
+  const token =
+    req.headers.authorization && req.headers.authorization.split('Bearer ')[1];
+  let userId = null;
+  let role = null;
+
+  if (token) {
+    try {
+      const decodedToken = jwt.verify(token, SECRET_KEY);
+      userId = decodedToken.id;
+      role = decodedToken.role;
+    } catch (err) {
+      console.error('Invalid token');
+    }
+  }
+
+  // Setting the context on the req object
+  req.context = { userId, role };
+
+  next();
+});
+
 app.use('/auth', authRoutes);
 
-const httpServer = http.createServer(app);
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  introspection: true,
-  playground: true,
-  formatError: (err) => {
-    return {
-      message: err.message,
-      statusCode: err.extensions?.code || 'INTERNAL_SERVER_ERROR',
-    };
-  },
-});
+// books api
+app.get('/books', getBooks);
+app.post('/books', addBook);
+app.put('/books/:id', editBook);
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(async () => {
+  .then(() => {
     console.log('Connected to MongoDB..');
-
-    await server.start();
-    app.use(
-      '/graphql',
-      expressMiddleware(server, {
-        context: async ({ req }) => {
-          const token =
-            req.headers.authorization &&
-            req.headers.authorization.split('Bearer ')[1];
-          let userId = null;
-          let role = null;
-
-          if (token) {
-            try {
-              const decodedToken = jwt.verify(token, SECRET_KEY);
-              userId = decodedToken.id;
-              role = decodedToken.role;
-            } catch (err) {
-              throw new Error('Invalid token');
-            }
-          }
-
-          return { userId, role };
-        },
-      })
-    );
-
-    httpServer.listen(PORT, () => {
-      console.log(`🚀 GraphQL server ready at ${API_URL}/graphql`);
-      console.log(`REST server ready at ${API_URL}/auth`);
+    http.createServer(app).listen(PORT, () => {
+      console.log(`REST server ready at ${API_URL}`);
     });
   })
   .catch((err) => {
