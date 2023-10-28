e0021e7674d1ec23cc894a96a3f097b2c1c28539
rafiqulshopon <rafiqul.dev@gmail.com>
2023-10-28 14:17:13 +0600

    update delete user api

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -96,4 +96,26 @@ router.put('/books/:id', async (req, res) => {
   }
 });
 
+// Delete Book
+router.delete('/book/:id', async (req, res) => {
+  const { userId, role } = req.context || {};
+  const bookId = req.params.id;
+
+  if (!userId || !allowedRoles.includes(role)) {
+    return res.status(403).json({ message: 'Not authorized' });
+  }
+
+  try {
+    const book = await Book.findByIdAndDelete(bookId);
+
+    if (!book) {
+      return res.status(404).json({ message: 'Book not found.' });
+    }
+
+    return res.status(200).json({ message: 'Book deleted successfully.' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Failed to delete the book.' });
+  }
+});
+
 export default router;
